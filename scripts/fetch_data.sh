560eafee130fb383308977d85621534537798ee3
Brett Graham <graham@rowland.harvard.edu>
2011-07-07 09:08:55 -0400

    made fetch_data script verbose

--- scripts/fetch_data.sh
+++ scripts/fetch_data.sh
@@ -8,4 +8,4 @@ LOCAL_DIR='/data/'
 
 #rsync data from server to local dir
 # rsync -rt /Users/labuser/Documents/setup_data/* vnsl.rowland.org:/Volumes/Drobo2/vnsl/Data/Behavior/setup_data/
-rsync -rtu $DATA_SERVER:$SERVER_DIR $LOCAL_DIR
+rsync -rtuv $DATA_SERVER:$SERVER_DIR $LOCAL_DIR
