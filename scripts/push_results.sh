b189c68b9a53c3fe88e31be107540e6d4a0000de
Brett Graham <graham@rowland.harvard.edu>
2011-07-13 18:42:21 -0400

    updating scripts for slight reorganization of directories and adding push_results

--- scripts/push_results.sh
+++ scripts/push_results.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-DATA_SERVER='coxlabphysiology1.rowland.org'
+RESULTS_SERVER='soma2.rowland.org'
+REMOTE_RESULTS_DIR='/media/raid/results'
+REMOTE_DATA_DIR='/media/raid/data'
+
+LOCAL_RESULTS_DIR='/data/analyzed'
+LOCAL_DATA_DIR='/data/raw'
 
 SERVER_DIR='/Volumes/Storage/data/physiology/'
 
-LOCAL_DIR='/data/raw/'
+LOCAL_DIR='/data/'
 
 #rsync data from server to local dir
 # rsync -rt /Users/labuser/Documents/setup_data/* vnsl.rowland.org:/Volumes/Drobo2/vnsl/Data/Behavior/setup_data/
