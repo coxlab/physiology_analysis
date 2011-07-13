8ccf4c2d810709a9fda96ce5bd6010e86d148da1
Brett Graham <graham@rowland.harvard.edu>
2011-07-13 19:07:06 -0400

    typos

--- scripts/push_data.sh
+++ scripts/push_data.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 RESULTS_SERVER='soma2.rowland.org'
-REMOTE_DATA_DIR='/media/raid/data'
+REMOTE_DATA_DIR='/media/raid/data/'
 
-LOCAL_DATA_DIR='/data/raw'
+LOCAL_DATA_DIR='/data/raw/'
 
 rsync -rtuv $LOCAL_DATA_DIR $RESULTS_SERVER:$REMOTE_DATA_DIR
--- scripts/push_results.sh
+++ scripts/push_results.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 RESULTS_SERVER='soma2.rowland.org'
-REMOTE_RESULTS_DIR='/media/raid/results'
+REMOTE_RESULTS_DIR='/media/raid/results/'
 
-LOCAL_RESULTS_DIR='/data/results'
+LOCAL_RESULTS_DIR='/data/results/'
 
 #rsync data from server to local dir
 rsync -rtuv $LOCAL_RESULTS_DIR $RESULTS_SERVER:$REMOTE_RESULTS_DIR
