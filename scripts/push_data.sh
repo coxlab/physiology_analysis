cae3e36022e55aa8a53cac73f537ed5430441e72
Brett Graham <graham@rowland.harvard.edu>
2011-07-13 19:02:59 -0400

    more script updates

---
+++ scripts/push_data.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+RESULTS_SERVER='soma2.rowland.org'
+REMOTE_DATA_DIR='/media/raid/data'
+
+LOCAL_DATA_DIR='/data/raw'
+
+rsync -rtuv $LOCAL_DATA_DIR $REMOTE_DATA_DIR:$RESULTS_SERVER
--- scripts/push_results.sh
+++ scripts/push_results.sh
@@ -2,15 +2,8 @@
 
 RESULTS_SERVER='soma2.rowland.org'
 REMOTE_RESULTS_DIR='/media/raid/results'
-REMOTE_DATA_DIR='/media/raid/data'
 
-LOCAL_RESULTS_DIR='/data/analyzed'
-LOCAL_DATA_DIR='/data/raw'
-
-SERVER_DIR='/Volumes/Storage/data/physiology/'
-
-LOCAL_DIR='/data/'
+LOCAL_RESULTS_DIR='/data/results'
 
 #rsync data from server to local dir
-# rsync -rt /Users/labuser/Documents/setup_data/* vnsl.rowland.org:/Volumes/Drobo2/vnsl/Data/Behavior/setup_data/
-rsync -rtuv $DATA_SERVER:$SERVER_DIR $LOCAL_DIR
+rsync -rtuv $LOCAL_RESULTS_DIR $REMOTE_RESULTS_DIR:$RESULTS_SERVER
