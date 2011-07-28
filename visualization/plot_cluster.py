92b80dd62f5a647dbb7e7c32f548e35cd8f701ed
Brett Graham <graham@rowland.harvard.edu>
2011-07-27 23:52:04 -0400

    testing plot cluster

--- visualization/plot_cluster.py
+++ visualization/plot_cluster.py
@@ -30,14 +30,38 @@ baseline = baseline / float(len(stimtimer.get_all_times())) / 0.1
 print "Baseline firing rate: %.2f" % baseline
 
 visualResponses = {} # ordered like times in stimtimer
+stimReps = {}
 for st in stimtimer.times:
     visualResponses[st] = physio.caton_utils.get_n_spikes(stimtimer.times[st], spiketimes, 0.1, 0.2, timebase)
+    stimReps[st] = stimtimer.times[st]
 visualResponses = np.array(sorted(visualResponses.iteritems(), key=lambda x: x[0]))
-stimnames = np.array(stimtimer.get_stim_attr('intName'))
-stimposx = np.array(stimtimer.get_stim_attr('pos_x'))
-stimposy = np.array(stimtimer.get_stim_attr('pos_y'))
-stimsize = np.array(stimtimer.get_stim_attr('size_x'))
+stimReps = np.array(sorted(stimReps.iteritems(), key=lambda x: x[0]))
 
+stimnamelist = stimtimer.get_unique_stim_attr('intName')
+stimnames = np.array(stimtimer.get_stim_attr('intName'))
+
+def collate_responses(stimname, attr):
+    stimIs = np.where(stimnamelist == stimname)[0]
+    uniquelist = stimtimer.get_unique_stim_attr(attr)
+    values = np.array(stimtimer.get_stim_attr(attr))
+    resp = []
+    for us in uniquelist:
+        valIs = np.where(values == us)[0]
+        Is = np.union1d(valIs, stimIs)
+        resp.append([us, sum(visualResponses[Is]), sum(stimReps[Is])])
+    return resp
+
+resp = {}
+nameresps = {}
+for sn in stimnamelist:
+    # stim_name, n_spikes, n_reps
+    nameresps.append([sn, sum(visualResponses[stimnames == sn]), sum(stimReps[stimnames == sn])])
+    # stim_name attr : attr_name, n_spikes, n_reps
+    stimresps[sn]['x'] = collate_responses(sn,'pos_x')
+    stimresps[sn]['y'] = collate_responses(sn,'pos_y')
+    stimresps[sn]['s'] = collate_responses(sn,'size_x')
+
+print "Response by ID:", nameresps
 # 2. NORMALIZED response to each stimulus collapsed across variation (and corresponding driven firing rate)
 #   stimID ...
 #   [raster] [raster] ....
