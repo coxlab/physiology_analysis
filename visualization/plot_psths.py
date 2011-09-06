51df117e6d88a9f3e1962f7bd3989b8b1865d5ea
Brett Graham <graham@rowland.harvard.edu>
2011-09-06 17:21:42 -0400

    plotting

--- visualization/plot_psths.py
+++ visualization/plot_psths.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import logging, sys
+import logging, optparse, sys
 logging.basicConfig(level = logging.DEBUG)
 
 import numpy as np
@@ -8,14 +8,34 @@ import pylab as pl
 
 import physio
 
-plotWindow = [-0.25, .75]
-plotNBins = 20
-channel = 7
+parser = optparse.OptionParser(usage="usage: %prog [options] session")
+parser.add_option("-g", "--group", dest="group", default="name",
+                    help="Group stimuli by this variable: name, pos_x, pos_y, size_x...")
+parser.add_option("-b", "--before", dest="before", default=-0.25,
+                    help="Seconds before stimulus onset to plot")
+parser.add_option("-a", "--after", dest="after", default=0.75,
+                    help="Seconds after stimulus onset to plot")
+parser.add_option("-n", "--nbins", dest="nbins", default=20,
+                    help="Number of bins in histogram")
+parser.add_option("-c", "--channel", dest="channel", default=7,
+                    help="Channel to plot")
+
+(options, args) = parser.parse_args()
+if len(args) != 1:
+    parser.print_usage()
+    sys.exit(1)
+
+# plotWindow = [-0.25, .75]
+# plotNBins = 20
+# channel = 7
+# group = 'name'
 # nChannels = 32
 # depthOrdered = physio.channelmapping.position_to_tdt(range(nChannels))
 
-session = physio.session.load('K4_110720')
+session = physio.session.load(args[0])#'K4_110720')
 # session = physio.session.load('K4_110830')
+
+
 
 trialTimes, stimuli, _, _ = session.get_trials()
 nTrials = len(trialTimes)
@@ -27,29 +47,35 @@ for s in stimCounts.keys():
     logging.debug("\t%i presentations of %s" % (stimCounts[s],s))
 
 # get unique positions & sizes
-pxs = {}
-pys = {}
-sizes = {}
-names = {}
+values = []
 for s in uniqueStimuli:
-    pxs[s['pos_x']] = 1
-    pys[s['pos_y']] = 1
-    sizes[s['size_x']] = 1
-    names[s['name']] = 1
-names = sorted(names.keys())
-pxs = sorted(pxs.keys())
-pys = sorted(pys.keys())
-sizes = sorted(sizes.keys())
+    values[s[options.group]] = 1
+values = sorted(values.keys())
+# pxs = {}
+# pys = {}
+# sizes = {}
+# names = {}
+# for s in uniqueStimuli:
+#     pxs[s['pos_x']] = 1
+#     pys[s['pos_y']] = 1
+#     sizes[s['size_x']] = 1
+#     names[s['name']] = 1
+# names = sorted(names.keys())
+# pxs = sorted(pxs.keys())
+# pys = sorted(pys.keys())
+# sizes = sorted(sizes.keys())
 
 # generate x & y arrays
 # 120 total :-O
 #[name, :], [name, px, py[1], :], [name, px[1], py, :], [name, size, :]
 conditions = []
-for n in names:
-    conditions.append({'name' : n})
+for v in values:
+    conditions.append({condition : v})
+# for n in names:
+#     conditions.append({'name' : n})
 # conditions = uniqueStimuli
 # data = [(ch,cl) for ch in depthOrdered for cl in range(1,6)]
-data = [(channel, cl) for cl in range(1,6)]
+data = [(options.channel, cl) for cl in range(1,6)]
 
 
 subplotsWidth = len(conditions)
@@ -66,7 +92,7 @@ for (y, datum) in enumerate(data):
         trials, _, _, _ = session.get_trials(condition)
         spikes = session.get_spike_times(*datum)
         pl.subplot(subplotsHeight, subplotsWidth, subplotsWidth * y + x + 1)
-        physio.plotting.psth.plot(trials, spikes, plotWindow[0], plotWindow[1], plotNBins)
+        physio.plotting.psth.plot(trials, spikes, options.before, options.after, options.nbins)
         pl.axvline(0., color = 'k')
         pl.axvspan(0., 0.5, color = 'k', alpha = 0.1)
         
@@ -89,4 +115,4 @@ for y in xrange(subplotsHeight):
         pl.subplot(subplotsHeight, subplotsWidth, subplotsWidth * y + x + 1)
         pl.ylim(0,ymaxs[y])
         
-pl.savefig("psth.png")
+pl.savefig("psth_%i_%s.png" % (options.channel, options.group))
