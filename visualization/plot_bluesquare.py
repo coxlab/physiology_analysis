f1a34fc09301bf838170a8d79158799b87fa951b
Brett Graham <graham@rowland.harvard.edu>
2011-09-12 16:27:26 -0400

    fixing bluesquare plot

--- visualization/plot_bluesquare.py
+++ visualization/plot_bluesquare.py
@@ -42,6 +42,7 @@ subplotsHeight = len(clusters)
 pl.figure(figsize=(subplotsWidth*2, subplotsHeight*2))
 # pl.gcf().suptitle('%s %d' % (groupBy, group))
 pl.subplot(subplotsHeight, subplotsWidth,1)
+pl.subplots_adjust(left = 0.025, right = 0.975, top = 0.9, bottom = 0.1, wspace = 0.45)
 logging.debug("Plotting %i by %i plots(%i)" % (subplotsWidth, subplotsHeight, subplotsWidth * subplotsHeight))
 
 for (y, cluster) in enumerate(clusters):
@@ -50,16 +51,16 @@ for (y, cluster) in enumerate(clusters):
         spikes = session.get_spike_times(channel, cluster)
         pl.subplot(subplotsHeight, subplotsWidth, subplotsWidth * y + x + 1)
         physio.plotting.psth.plot(trialTimes, spikes, options.before, options.after, options.nbins)
-        physio.plotting.raster.plot(trialTimes, spikes, options.before, options.after)
+        #physio.plotting.raster.plot(trialTimes, spikes, options.before, options.after)
         pl.axvline(0., color = 'k')
         pl.axvspan(0., 0.5, color = 'k', alpha = 0.1)
         
         if x == 0:
-            pl.ylabel('Cl: %i\nRate(Hz)' % cluster)
-        else:
-            pl.yticks([])
+            pl.ylabel('Cluster: %i\nRate(Hz)' % cluster)
+        #else:
+        #    pl.yticks([])
         if y == 0: pl.title('Ch:%i' % channel, rotation=45)
-        if y < len(data) - 1:
+        if y < len(clusters) - 1:
             pl.xticks([])
         else:
             pl.xticks([0., .5])
