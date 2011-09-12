ea7d97ac1ffdcaa6c7291a4e95e854a58b88e8f8
Brett Graham <graham@rowland.harvard.edu>
2011-09-12 17:47:47 -0400

    work on position finding

--- physio/session.py
+++ physio/session.py
@@ -152,7 +152,10 @@ class Session(object):
         return goodTimes, goodStims, badTimes, badStims
     
     def get_channel_locations(self):
-        pass
+        cncDict = events.cnc.get_cnc_events(self._file)
+        offset = events.cnc.get_tip_offset(self._file):
+        time, _ = self.get_epoch_time_range('mworks')
+        return events.cnc.get_channel_locations(cncDict, offset, time):
     
     def get_blackouts(self):
         pass
