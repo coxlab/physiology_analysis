2c3d31a20babe7b4622a483f7e9ae914bea591e9
Brett Graham <graham@rowland.harvard.edu>
2011-09-13 16:26:04 -0400

    adding combined run/plot script, accepts variable length session arguments

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+plotdir="/home/graham/Repositories/coxlab/physiology_analysis/visualization/"
+
+pushd $plotdir
+
+for session in "$@"
+do
+    echo "physio.py -f $1"
+    physio.py -f $1
+    echo "plot.sh $1"
+    plot.sh $1
+done
+
+popd
