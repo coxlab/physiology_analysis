a915764c5c5659d888dfa9611603dd8a34233b3b
Brett Graham <graham@rowland.harvard.edu>
2011-10-11 10:53:53 -0400

    adding icapp to run script

--- run.sh
+++ run.sh
@@ -6,8 +6,15 @@ cd $plotdir
 
 for session in "$@"
 do
+    # run icapp.py on audio files
+    python ~/Repositories/braingram/icapp/icapp.py -m random -s 441000 -o /scratch/$session/Audio\ Files/ /data/raw/$session/Audio\ Files/input_*
+    # copy over other files
+    cp /data/raw/$session/Audio\ Files/pixel* /scratch/$session/Audio\ Files/
+    cp /data/raw/$session/$session.h5 /scratch/$session/
     echo "phy.py -f $session"
     phy.py -f $session
     echo "plot.sh $session"
     sh plot.sh $session
+    # clean up
+    # TODO
 done
