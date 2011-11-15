0efb5960d93d8e4f5a0b98908f37830ad09f5b14
Brett Graham <graham@rowland.harvard.edu>
2011-11-15 15:46:53 -0500

    adding response vs id to plot_driven

--- visualization/plot_driven_ff.py
+++ visualization/plot_driven_ff.py
@@ -2,14 +2,14 @@
 
 import logging, optparse, os, sys
 logging.basicConfig(level = logging.DEBUG)
-import matploblib
+#import matplotlib
 import numpy as np
-import pylab as plt
+import pylab as pl
 
 import physio
 
-rwin = (100,200)
-bwin = (-100,0)
+rwin = (0.1,0.2)
+bwin = (-0.1,0)
 
 sessionName = 'K4_110804'
 epochNumber = 0
@@ -36,7 +36,7 @@ assert len(trialTimes) + len(targetTimes) == len(allStimTimes)
 
 # get driven firing rate
 bspikes = physio.spikes.stats.event_lock(allStimTimes, spikes, bwin[0], bwin[1], unpack=True)
-brate = len(bspikes) / float(bwin[1] - bwin[0])
+brate = len(bspikes) / (float(bwin[1] - bwin[0]) * len(allStimTimes))
 print "Baseline rate: %f spikes per second" % brate
 
 # get all stim ids, stim positions, stim sizes
@@ -61,16 +61,34 @@ idRates = {}
 for stimId in ids:
     trials, _, _, _ = session.get_trials({'name' : stimId})
     sspikes = physio.spikes.stats.event_lock(trials, spikes, rwin[0], rwin[1], unpack=True)
-    idRates[stimId] = len(sspikes) / float(rwin[1] - rwin[0])
+    idRates[stimId] = len(sspikes) / (float(rwin[1] - rwin[0]) * len(trials))
 print "ID Rates:", idRates
 idByRate = sorted(idRates, key=idRates.get)
 minId = idByRate[0]
 maxId = idByRate[-1]
-midId = idByRate[int(len(idByRate)/2)]
+midI = int(len(idByRate)/2)
+midId = idByRate[midI]
 print "Min, mid, max:", minId, midId, maxId
+
+pl.figure()
+pl.subplot(211)
+pl.subplot(224)
+rates = [idRates[i] for i in idByRate]
+names = idByRate
+colors = ['k' for i in xrange(len(rates))]
+colors[0] = 'b'
+colors[midI] = 'g'
+colors[-1] = 'r'
+pl.vlines(range(len(rates)), pl.zeros(len(rates)), rates, colors=colors, lw=5.0)
+pl.xticks(range(len(rates)), names)
+pl.ylabel("Firing Rate (Hz)")
+pl.axhline(brate)
+pl.xlabel("Stimulus Id")
+pl.xlim((-1,len(rates)+1))
 
 # collapse across position (plot size)
 
 # collapse across size (plot position)
 
 # save plot
+pl.savefig("test.png")
