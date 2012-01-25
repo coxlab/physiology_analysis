d6c50633f06449921307d742c323221ee80e95dd
Brett Graham <graham@rowland.harvard.edu>
2012-01-25 13:32:25 -0500

    tested plot_spike_trains, how generates a plot of time ranges where spikes were seen per channel per cluster relative to epoch start and stop

--- visualization/plot_spike_trains.py
+++ visualization/plot_spike_trains.py
@@ -1,31 +1,34 @@
 #!/usr/bin/env python
 
 import logging
-import argparse
 import os
 import sys
 logging.basicConfig(level = logging.DEBUG)
 
-import numpy as np
+#import numpy as np
 import pylab as pl
 
 import physio
 
 # parse command line options
-parser = argparse.ArgumentParser()
-parser.add_option("session")
-parser.add_option("epoch", default=None)
+#parser = argparse.ArgumentParser()
+#parser.add_argument("session")
+#parser.add_argument("epoch", default=None)
 
-args = parser.parse_args()
+#args = parser.parse_args()
+if len(sys.argv) < 2:
+    raise ValueError("Must supply session: script.py <session>")
+    sys.exit(1)
+session_name = sys.argv[1]
 
-config = physio.cfg.load(args.session)
+config = physio.cfg.load(session_name)
 
-if args.epoch is None:
+if len(sys.argv) < 3:
     epochs = range(physio.session.get_n_epochs(config))
 else:
-    epochs = [int(args.epoch),]
+    epochs = [int(sys.argv[2]),]
 
-session_name = args.session
+#session_name = args.session
 
 for epochNumber in epochs:
     session = physio.session.load(session_name, epochNumber)
@@ -34,7 +37,7 @@ for epochNumber in epochs:
     #trialTimes, stims = session.get_stimuli(stimType = 'image')
     #nTrials = len(trialTimes)
     time_range = session.get_epoch_time_range('au')
-    pl.figure(1, figsize=(8,8))
+    pl.figure(1, figsize=(8,16))
     pl.axvline(time_range[0], color='k')
     pl.axvline(time_range[1], color='k')
 
@@ -43,21 +46,27 @@ for epochNumber in epochs:
 
     cmap = pl.cm.Paired
 
+    dy = 1/float(max_n_clusters-1)
     for ch in xrange(1,33):
         n_clusters = session.get_n_clusters(ch)
+        if ch % 2: # highlight every other channel
+            pl.bar(time_range[0], 1, time_range[1]-time_range[0], ch,\
+                    color = 'k', alpha=0.2, linewidth = 0)
         for cl in xrange(n_clusters):
-
-            dy = cl/float(n_clusters-1)
-            color = cmap(cl/float(max_n_clusters-1))
-
             spikes = session.get_spike_times(ch, cl)
+            if len(spikes) == 0: continue
 
-            dy = cl/float(n_clusters-1)
-            c = cmap(cl/float(max_n_clusters-1))
-            pl.bar(spikes[0], dy, spikes[-1]-spikes[0], ch,\
+            c = cmap(cl * dy)
+            pl.bar(spikes[0], dy, spikes[-1]-spikes[0], ch + dy * cl,\
                     color = c, linewidth = 0)
+        lx = time_range[0] - 10
+        ly = ch + .5
+        pl.text(lx, ly, "%i" % ch, ha='right', va='center')
 
     # TODO make sensible axes
+    pl.xlabel("Time (Seconds, audio)")
+    pl.yticks([])
+    pl.ylim([1,33])
     session.close()
 
     if not os.path.exists(outdir): os.makedirs(outdir)
