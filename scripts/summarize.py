4b148645b409ab172d6268228360cc30c1d6b730
Brett Graham <graham@rowland.harvard.edu>
2012-05-31 10:22:11 -0400

    parallelizing summarize

--- scripts/summarize.py
+++ scripts/summarize.py
@@ -1,13 +1,37 @@
 #!/usr/bin/env python
 
 import logging
+import sys
+
+import joblib
 
 import physio
 
-for session_name in physio.session.get_sessions():
-    logging.debug("\tprocessing %s" % session_name)
+n_jobs = -1
+
+
+def try_summarize(sn):
+    logging.debug("\tprocessing %s" % sn)
     try:
-        physio.summary.summarize.summarize_session(session_name)
+        physio.summary.summarize.summarize_session(sn)
     except Exception as E:
         logging.error("Session %s failed with %s" % \
-                (session_name, str(E)))
+                (sn, E))
+
+
+def get_sessions():
+    return physio.session.get_sessions()
+
+
+def summarize(sns, n_jobs):
+    joblib.Parallel(n_jobs=n_jobs)(joblib.delayed(try_summarize)(sn) \
+            for sn in sns)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    args = sys.argv[1:]
+    if len(args):
+        summarize(args, n_jobs)
+    else:
+        summarize(get_sessions(), n_jobs)
