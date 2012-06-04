64db0ce7e6f3131f553344cbf701c0bba86ad739
David Cox <david.daniel.cox@gmail.com>
2012-06-04 17:16:48 -0400

    Add a basic sliding-window-style latency measure

--- physio/spikes/triallock.py
+++ physio/spikes/triallock.py
@@ -5,6 +5,58 @@ import numpy
 import latency
 
 
+def sliding_window_rate(spikes, trials, prew, duration, width, stride, timebase='middle'):
+
+    bin_starts = numpy.arange(-prew, duration, stride)
+    bin_ends = bin_starts + width
+
+    trial_rates = numpy.empty((len(trials), bin_starts.shape[0]))
+
+    def match_windows(spike_time):
+        # for a given spike time, this will return a logical array
+        # with True in every window/bin the spike is in, and
+        # False in every bin it is not
+        return (spike_time > bin_starts) & (spike_time <= bin_ends)
+
+    def sum_matched_windows(a, b):
+        return a + b
+
+    for trial_num, trial in enumerate(trials):
+        start = trial - prew
+        end = trial + duration
+        ts = spikes[numpy.logical_and(spikes > start, spikes <= end)] \
+                - trial
+
+        mapped = map(match_windows, ts)
+
+        if len(mapped) > 0:
+            trial_rates[trial_num, :] = reduce(lambda x, y: x + y, mapped)
+
+    if timebase is 'start':
+        times = bin_starts
+    elif timebase is 'end':
+        times = bin_ends
+    elif timebase is 'middle':
+        times = (bin_starts + bin_ends) / 2.0
+    else:
+        raise Exception('Unknown timebase argument')
+
+    return sum(trial_rates, 1) / (width * len(trials)), times
+
+
+def baseline_normalized_sliding_window_rate(spikes, trials, prew, duration, width, stride):
+
+    time_course, times = sliding_window_rate(spikes, trials, prew, duration, width, stride, timebase='end')
+
+    mean_baseline = numpy.mean(time_course[times < 0.0])
+    baseline_subtracted = time_course - mean_baseline
+    std_baseline = numpy.std(baseline_subtracted[times < 0.0])
+
+    normalized = baseline_subtracted / std_baseline
+
+    return normalized, times
+
+
 def bin_response(spikes, trials, prew, duration, binw, raw=False):
     """
     Parameters
